aca3b140cb5759858e69f682952d567b9a4b03fe
Nishant Patel <nishantuwindsor@gmail.com>
2018-04-22 15:02:52 -0400

    Use dataBinding library

--- app/src/main/java/com/example/android/cdocs/ui/LoginActivity.java
+++ app/src/main/java/com/example/android/cdocs/ui/LoginActivity.java
@@ -1,15 +1,18 @@
 package com.example.android.cdocs.ui;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.android.cdocs.R;
+import com.example.android.cdocs.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        ActivityLoginBinding loginActivityBinging =
+                DataBindingUtil.setContentView(this, R.layout.activity_login);
     }
 }
