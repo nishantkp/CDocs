cbeef21c67dd7cdf156d4dee1df9f8cd6fa505c3
Nishant Patel <nishantuwindsor@gmail.com>
2018-04-24 00:33:42 -0400

    Create BaseActivity

---
+++ app/src/main/java/com/example/android/cdocs/base/BaseActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.cdocs.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.cdocs.data.DataManager;
+
+public class BaseActivity extends AppCompatActivity {
+    public DataManager mDataManager;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mDataManager = DataManager.getInstanace(this);
+    }
+}
