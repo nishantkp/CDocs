e293d7284fc1d7c109687b6b4c169bf648ab8282
Nishant Patel <nishantuwindsor@gmail.com>
2018-04-25 19:45:58 -0400

    Configure app to receive FCM notification

---
+++ app/src/main/java/com/example/android/cdocs/service/FcmInstanceIdService.java
@@ -0,0 +1,11 @@
+package com.example.android.cdocs.service;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * This is required if you want to do any message handling beyond receiving notifications on apps
+ * in the background. To receive notifications in foregrounded apps, to receive data payload,
+ * to send upstream messages, and so on
+ */
+public class FcmInstanceIdService extends FirebaseInstanceIdService {
+}
---
+++ app/src/main/java/com/example/android/cdocs/service/FcmService.java
@@ -0,0 +1,10 @@
+package com.example.android.cdocs.service;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * to handle the creation, rotation, and updating of registration tokens.
+ * This is required for sending to specific devices or for creating device groups.
+ */
+public class FcmService extends FirebaseMessagingService {
+}
