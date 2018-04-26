2d40d822e53508cd81249a3be8535f53f6a11b97
Nishant Patel <nishantuwindsor@gmail.com>
2018-04-25 20:42:51 -0400

    Insert item into database when notification is received

--- app/src/main/java/com/example/android/cdocs/service/FcmService.java
+++ app/src/main/java/com/example/android/cdocs/service/FcmService.java
@@ -1,10 +1,24 @@
 package com.example.android.cdocs.service;
 
+import com.example.android.cdocs.data.DataManager;
+import com.example.android.cdocs.ui.model.Docs;
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
 /**
  * to handle the creation, rotation, and updating of registration tokens.
  * This is required for sending to specific devices or for creating device groups.
  */
 public class FcmService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        if (remoteMessage.getData().size() > 0) {
+            DataManager dataManager = DataManager.getInstance(this);
+            // Insert single item into database
+            dataManager.insertSingleItemToDatabase(
+                    new Docs(
+                            remoteMessage.getData().get("TYPE"),
+                            remoteMessage.getData().get("TYPE")));
+        }
+    }
 }
