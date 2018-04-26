55f2713674842c211bf6d4f7724ca03dc447e410
Nishant Patel <nishantuwindsor@gmail.com>
2018-04-25 21:53:34 -0400

    Add stetho

--- app/src/main/java/com/example/android/cdocs/application/CdocApplication.java
+++ app/src/main/java/com/example/android/cdocs/application/CdocApplication.java
@@ -2,6 +2,7 @@ package com.example.android.cdocs.application;
 
 import android.app.Application;
 
+import com.facebook.stetho.Stetho;
 import com.twitter.sdk.android.core.Twitter;
 
 public class CdocApplication extends Application {
@@ -9,5 +10,6 @@ public class CdocApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Twitter.initialize(this);
+        Stetho.initializeWithDefaults(CdocApplication.this);
     }
 }
