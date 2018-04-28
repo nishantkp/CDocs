35d376ff762e9381a0422d607c6a31bb5a4ec164
Nishant Patel <nishantuwindsor@gmail.com>
2018-04-28 18:52:30 -0400

    Create Pdf activity

---
+++ app/src/main/java/com/example/android/cdocs/ui/pdf/PdfActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.cdocs.ui.pdf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.cdocs.base.BaseActivity;
+
+public class PdfActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdf);
+    }
+}
